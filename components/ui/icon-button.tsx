6c854a8f8afcf56791f79839247a736cf1a087bc
Klemen <klemen.nedelko2@gmail.com>
2023-08-22 15:22:27 +0200

    product page and product modal

---
+++ components/ui/icon-button.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/lib/utils'
+import React, { MouseEventHandler } from 'react'
+
+interface IconButtonProps{
+    className?: string
+    icon: React.ReactElement
+    onClick?: MouseEventHandler<HTMLButtonElement> | undefined
+}
+
+const IconButton: React.FC<IconButtonProps> = ({className, onClick, icon}) => {
+  return (
+    <button onClick={onClick} className={cn("rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition", className)}>
+        {icon}
+    </button>
+  )
+}
+
+export default IconButton
