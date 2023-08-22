5835955f8099921261322d0649c486520bf8d189
Klemen <klemen.nedelko2@gmail.com>
2023-08-22 20:31:44 +0200

    version 1.0

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -15,6 +15,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
         rounded-full bg-black
         border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 trasition`, className)} 
         ref={ref}
+        disabled={disabled}
         {...props}
         >
             {children}
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -8,6 +8,7 @@ import { Expand, ShoppingCart } from 'lucide-react'
 import Currency from './currency'
 import { useRouter } from 'next/navigation'
 import usePreviewModal from '@/hooks/use-preview-modal'
+import useCart from '@/hooks/use-cart'
 
 interface ProductCardProps{
     data: Product
@@ -15,6 +16,7 @@ interface ProductCardProps{
 
 const ProductCard:React.FC<ProductCardProps> = ({data}) => {
     const previewModal = usePreviewModal()
+    const cart = useCart()
     const router = useRouter()
     const handleClick = () =>{
         router.push(`/product/${data?.id}`)
@@ -24,6 +26,11 @@ const ProductCard:React.FC<ProductCardProps> = ({data}) => {
         event.stopPropagation()
         previewModal.onOpen(data)
     }
+    const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event)=>{
+        event.stopPropagation()
+        cart.addItem(data)
+    }
+    
   return (
     <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4" onClick={handleClick}>
         <div className="aspec-square rounded-xl bg-gray-100 relative  w-full h-64">
@@ -31,7 +38,7 @@ const ProductCard:React.FC<ProductCardProps> = ({data}) => {
             <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                 <div className="flex gap-x-6 justify-center">
                     <IconButton onClick={onPreview} icon={<Expand size={20} className="text-grap-600"/>}/>
-                    <IconButton onClick={()=>{}} icon={<ShoppingCart size={20} className="text-grap-600"/>}/>
+                    <IconButton onClick={onAddToCart} icon={<ShoppingCart size={20} className="text-grap-600"/>}/>
                 </div>
             </div>
         </div>    
